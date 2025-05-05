90461887ba1b2f75e3e82b3c9017e7357c2a96af
Daren Morreale <daren@givelegacy.com>
2025-05-04 21:21:42 -0500

    added single product

--- store/src/components/ProductDetail.jsx
+++ store/src/components/ProductDetail.jsx
@@ -3,36 +3,39 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductDetail() {
-  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState(null);
   const { id } = useParams();
-  console.log(id)
+  console.log(id);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch("http://localhost:3033/api/products");
+        const response = await fetch(
+          `http://localhost:3033/api/product/${id}`
+        );
         const result = await response.json();
         // console.log(result);
-        setProducts(result);
+        setProduct(result);
       } catch (error) {
         console.log("Error in fetchData", error);
       }
     }
-
     fetchData();
-  }, []);
+  }, [id]);
+
+      if (!product) {
+        return <p>Loading...</p>;
+      }
 
   return (
     <div className="product">
       <h1>Your product</h1>
-      {products.map((product) => (
-        <div>
-          <h3>{product.description}</h3>
-          <h4>{product.name}</h4>
-          <p>Price: ${product.price}</p>
-          <img src={product.img_url} style={{ Width: "150px" }} />
-        </div>
-      ))}
+      <div>
+        <h3>{product.description}</h3>
+        <h4>{product.name}</h4>
+        <p>Price: ${product.price}</p>
+        <img src={product.img_url} style={{ Width: "150px" }} />
+      </div>
     </div>
   );
 }
